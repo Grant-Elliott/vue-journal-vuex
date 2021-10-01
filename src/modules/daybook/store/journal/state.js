bf1ed2a538198272ae73f490dc0222550a16ad03
Grant Elliott <grante48@hotmail.com>
2021-10-01 12:57:55 +0200

    First commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Pariatur culpa mollit dolor et nostrud nostrud. Dolore laboris cupidatat commodo labore consequat eiusmod eu enim ea irure aute dolor. Commodo nisi adipisicing exercitation sit esse laboris. Incididunt pariatur enim enim duis elit ullamco excepteur aliquip do cillum.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text: 'Aliquip mollit Lorem elit enim culpa in sunt magna et. Deserunt aliqua tempor esse commodo do exercitation do duis incididunt ipsum. Ad dolore aliqua Lorem est in eu reprehenderit. Reprehenderit eu quis occaecat elit ipsum amet qui adipisicing voluptate velit et excepteur velit ad. Ex cillum consequat sit in. Amet do exercitation in enim nostrud cupidatat aliquip proident ad incididunt.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime()+2000,
+            date: new Date().toDateString(),
+            text: 'Amet ipsum consequat voluptate aute do est exercitation eiusmod. Commodo in aute sint fugiat. Consectetur excepteur pariatur fugiat ullamco. Minim et anim dolore commodo. Fugiat incididunt in ipsum aliqua esse sint. Cupidatat ullamco do ullamco incididunt ex velit non commodo. Qui proident elit dolore reprehenderit excepteur nostrud ipsum aute quis voluptate non cillum eiusmod.',
+            picture: null,
+        },
+
+    ]
+})
